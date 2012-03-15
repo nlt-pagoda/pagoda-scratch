f2cdef92ecb7b7ee80d696e8ad768c1bbec867c7
Alex Oulapour <DaftAlex@DaftAlex7.(none)>
2012-03-15 00:35:41 -0500

    - Added Remove button to view_usersingle.php page. When viewing user profile there is not a Remove button that will delete the user.

--- view/admin/view_usersingle.php
+++ view/admin/view_usersingle.php
@@ -2,8 +2,15 @@
 
 <h1>Admin Control Panel</h1>
 
+<?php 
+if (isset($removed)):
+	$this->RenderMsg("User Removed")
+?>
 
-<?php if (isset($singleton)): ?>
+
+<?php 
+else: 
+if (isset($singleton)): ?>
 <h2><?php 
 echo $user[0]["User"]["username"];
 ?></h2>
@@ -23,10 +30,19 @@ echo $user[0]["User"]["username"];
 	echo "<td><strong>Role:  </strong></td><td>".$role[0]["Role"]["role"]."</td><br/>";
 	echo "</tr>";
 	echo "</table>";
+	
+	//remove button
+	echo("<form action=\"\" method=\"POST\">");
+	echo("<input type=\"hidden\" name=\"UserID\" value=\"".$user[0]["User"]["UserID"]."\" />");
+	echo("<input type=\"submit\" name=\"remove\" value=\"Remove\"/>");	
+	echo("</form>");
+			
 ?>
 
 
-<?php endif; ?>
+<?php 
+endif;
+endif; ?>
 
 <?php else: 
 $this->RenderMsg("You do not have sufficient privileges to view this page!"); ?>
