298edd07eb4edd7ed543ff7a227d5170130944e6
Alex Oulapour <DaftAlex@DaftAlex7.(none)>
2012-03-17 05:52:12 -0500

    - Made all info required when signing up users since leaving info out will cause more problems down the line due to users being listed by name. It's best just to get all the info in there at once.
    - added Class Size to instructor/view_courses.php and
    student/view_courses.php
    
    - removed extraneous code from admin/add/announcement
    
    - Instructor can add and view announcements to courses. Login with
    instructor, go to instructor/view/course/$num to see. Only shows top
    2 announcements right now. Will add link to show more later. Layout is
    a bit sloppy right now.
    
    - Instructors have remove button for each announcement

--- view/instructor/view_course.php
+++ view/instructor/view_course.php
@@ -1,8 +1,50 @@
 <?php if ($accessible): ?>
 
+<?php if (isset($removed)):
+header('Location:'.$courseID);
+endif ?>
+
+<h1>Instructor Control Panel</h1>
+
 <h2><?php 
 echo $course[0]["Course"]["name"];
 ?></h2>
+<span><h2 style="line-height:0">Announcements</h2>
+<a href="<?php echo BASEPATH; ?>instructor/add/announcement/<?php echo $course[0]["Course"]["CourseID"];?>">Add Announcement</a></span>
+
+<div id="headlinesList">
+<?php
+$announcementsLength = count($announcements);
+
+	for($i=0;$i<$announcementsLength;$i++)
+	{
+		if($i==0)
+		{
+			echo("<div id=\"latestHeadline\">&bull; <span id=\"latestHeadlineTitle\">".$announcements[$i]["Announcement"]["title"]."</span>"."<span id=\"latestHeadlineDate\"> ".$announcements[$i]["Announcement"]["date"]."</span>");
+			echo("<div id=\"latestHeadlineText\">".$announcements[$i]["Announcement"]["text"]."</div></div>");
+			
+			//remove button
+			echo("<form action=\"\" method=\"POST\">");
+			echo("<input type=\"hidden\" name=\"AnnouncementID\" value=\"".$announcements[$i]["Announcement"]["AnnouncementID"]."\" />");
+			echo("<input type=\"submit\" name=\"remove\" value=\"Remove\"/>");	
+			echo("</form>");
+			echo("<h1></h1>");
+		}
+		else 
+		{
+			echo("<div id=\"headline\">&bull; <span id=\"headlineTitle\">".$announcements[$i]["Announcement"]["title"]."</span>"."<span id=\"headlineDate\"> ".$announcements[$i]["Announcement"]["date"]."</span>");
+			echo("<div id=\"headlineText\">".$announcements[$i]["Announcement"]["text"]."</div></div>");
+			
+			//remove button
+			echo("<form action=\"\" method=\"POST\">");
+			echo("<input type=\"hidden\" name=\"AnnouncementID\" value=\"".$announcements[$i]["Announcement"]["AnnouncementID"]."\" />");
+			echo("<input type=\"submit\" name=\"remove\" value=\"Remove\"/>");	
+			echo("</form>");
+			echo("<h1></h1>");
+		}
+	}
+?>
+</div>
 
 <?php
 	echo "<table>";
@@ -16,7 +58,7 @@ echo $course[0]["Course"]["name"];
 	echo "<td><strong>CRN:  </strong></td><td>".$course[0]["Course"]["CRN"]."</td><br/>";
 	echo "</tr>";
 	echo "<tr>";
-	echo "<td><strong>Class Size:  </strong></td><td>PLACEHOLDER</td><br/>";
+	echo "<td><strong>Class Size:  </strong></td><td>".$studentCount[0][""]["COUNT(StudentID)"]."</td><br/>";
 	echo "</tr>";
 	echo "<tr>";
 	echo "<td><strong>Students:  </strong></td><td>PLACEHOLDER</td><br/>";
