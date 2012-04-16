c78cabda045aa77814e6558348b564bb20934f1d
Alex Oulapour <DaftAlex@DaftAlex7.(none)>
2012-04-16 10:07:33 -0500

    4-16-12 -Got creating a viewing a rubric working

--- view/instructor/create_assessment.php
+++ view/instructor/create_assessment.php
@@ -17,33 +17,43 @@ endif ?>
 <form action="" method="POST">
 	<table>
 	<tr>
-	<td><label for="name">Assessment Name:  </label></td><td><input type="text" name="name" /></td>
+	<td><label for="name">Assessment Name:  </label></td><td><input type="text" name="assessmentName" /></td>
 	</tr>
 	<tr>
 	<td></td>
-	<td><input type="submit" name="submit" value="submit"/></td>
+	<td><input type="submit" name="submit" value="submit" onclick = "setHTML()" /></td>
+	</tr>
+	<tr>
+	<td><label for="name">Rubric Name:  </label></td><td><input type="text" name="rubricName" /></td>
 	</tr>
 	</table>
 	
-	<br><br>
 	<h2>Rubric:</h2>
-	<input type="button" value="Add Score (Column)" onclick="addColumn()">
-	<input type="button" value="Add Criteria (Row)" onclick="addTableRow($('#rubricTable'))">
+	<input type="button" value="Add Score (Column)" onclick="addColumn()" />
+	<input type="button" value="Add Criteria (Row)" onclick="addTableRow($('#rubricTable'))" />
+	<span id="rvalues">Rows: 1</span>
+	<span id="cvalues">Columns: 1</span>
 	<table id="rubricTable" border="1">
-	<tr><td></td><td>Score</td></tr>
-	<tr><td>Criteria</td><td><textarea>Criteria description to achieve Score</textarea></td></tr>
+	<tbody>
+	<tr><td></td><td><span class="editable1">Score</span></td></tr>
+	<tr><td><span class="editable1">Criteria</span></td><td><span class="editable1">Click to edit</span></td></tr>
+	</tbody>
 	</table>
+	<div id="tableHTML" style="display:none"></div>
+
 </form>
 	
 	<script>
 	function addTableRow(jQtable){
 	    jQtable.each(function(){
+			var numRows = $("#rubricTable").find('tr').length;
+		    
 	        var $table = $(this);
 	        // Number of td's in the last table row
 	        var n = $('tr:last td', this).length;
-	        var tds = '<tr><td>Criteria</td>';
+	        var tds = '<tr><td><span class="editablerow' + numRows + '">Criteria</span></td>';
 	        for(var i = 0; i < n-1; i++){
-	            tds += '<td><textarea>Criteria description to achieve Score</textarea></td>';
+	            tds += '<td><span class="editablerow' + numRows + '">Click to edit</span></td>';
 	        }
 	        tds += '</tr>';
 	        if($('tbody', this).length > 0){
@@ -51,13 +61,71 @@ endif ?>
 	        }else {
 	            $(this).append(tds);
 	        }
+	        rvalues(numRows);
+	  	  setEditableRows(numRows);
 	    });
+
+
 	}
 
 	function addColumn()
 	{
-		$("#rubricTable tr:first").append("<td>Score</td>");
-		$("#rubricTable tr:gt(0)").append("<td><textarea>Criteria description to achieve Score</textarea></td>");
+		//count columns
+		var numCols = $("#rubricTable").find('tr')[0].cells.length;
+
+		var scoreHeader = '<td><span class="editablecol' + numCols + '">Score</span></td>';
+		$("#rubricTable tr:first").append(scoreHeader);
+		$("#rubricTable tr:gt(0)").append('<td><span class="editablecol' + numCols + '">Click to edit</span></td>');
+		cvalues(numCols);
+
+		var html = $('#rubricTable')[0].outerHTML;
+
+		setEditableCols(numCols);
+
+	}
+
+	function cvalues(num)
+	{
+		document.getElementById('cvalues').innerHTML ='Columns: ' + num;
+
+	}
+	function rvalues(num)
+	{
+		document.getElementById('rvalues').innerHTML ='Rows: ' + num;
+	}
+
+	function setHTML()
+	{
+		var html = $('#rubricTable')[0].outerHTML;
+		document.getElementById('tableHTML').innerHTML ='<textarea name="tableHTML">' + html + '</textarea>';
+	}
+
+
+	setEditable();
+			
+	function setEditable()
+	{
+		$(function(){
+			$('.editable1').editable();
+			});
+
+	}
+	
+
+	function setEditableRows(num)
+	{
+		$(function(){
+			$('.editablerow' + num).editable();
+			});
+
+	}
+
+	
+	function setEditableCols(num)
+	{
+		$(function(){
+			$('.editablecol' + num).editable();
+			});
 
 	}
 	</script>
--- view/instructor/view_assessment.php
+++ view/instructor/view_assessment.php
@@ -1,5 +1,7 @@
 <?php if ($accessible): ?>
 
+
+
 <div id=sidepanel>
 	<h2>Controls</h2>
 	<ul>
@@ -7,16 +9,16 @@
 	<li><a href="<?php echo BASEPATH; ?>instructor/create/assessment/">Create Assessment</a></li>
 	</ul>
 </div>
- 
-<h1>Assessments</h1>
-<ul>
+
+<h1>View Assessment</h1>
+
+<h2><?php 
+echo $assessment[0]["Assessment"]["name"];
+?></h2>
+
 <?php 
-foreach ($assessments as $assessment)
-	echo("<li><a href=\"".BASEPATH."instructor/view/assessment/".$assessment["Assessment"]["AssessmentID"]."\">".$assessment["Assessment"]["name"]."</a><br/></li>");
+echo $rubricHTML[0]["Rubric"]["html"];
 ?>
-</ul>
-
-
 
 
 <?php else: 
